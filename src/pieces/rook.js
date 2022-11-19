ffae5592685b54f9098fcadbcd553e55fabef5a1
Joichiro <Joichiro@wf.local>
2022-11-19 14:51:36 +0800

    finish the logic of rooks move

--- src/pieces/rook.js
+++ src/pieces/rook.js
@@ -22,51 +22,66 @@ export const rookMoveTo = (board, row, col, firstMove, pieceMove, legalMove, set
     }
     if(firstMove.row === row){
         for(let i = Math.min(col, firstMove.col); i < Math.max(col, firstMove.col); i++){
-            if(board[row][i] !== ""){
-                if(board[row][i] === pieceMove){
-                    haveInteraction = false;
-                }
-                else{
-                    haveInteraction = true;
+            if(!haveInteraction){
+                if(board[row][i] !== ""){
+                    if(board[row][i] === pieceMove){
+                        haveInteraction = false;
+                    }
+                    else{
+                        if(col === i)continue;
+                        haveInteraction = true;
+                    }
                 }
             }
-            console.log("same row",  board[row][i]);
+            else{
+                break;
+            }
         }
     }
     else{
         for(let i = Math.min(firstMove.row, row); i < Math.max(firstMove.row, row); i++){
-            if(board[i][col] !== ""){
-                if(board[i][col] === pieceMove){
-                    haveInteraction = false;
-                }
-                else{
-                    haveInteraction = true;
+            if(!haveInteraction){
+                if(board[i][col] !== ""){
+                    if(board[i][col] === pieceMove){
+                        haveInteraction = false;
+                    }
+                    else{
+                        if(row === i)continue;
+                        haveInteraction = true;
+                    }
                 }
             }
-            console.log("same col",  board[i][col]);
+            else{
+                break;
+            }
         }
     }
 
-    console.log(haveInteraction);
+    console.log(haveInteraction)
 
-    if(board[row][col] !== "" && !board[row][col].includes(pieceMove[0]) && validMove && !haveInteraction){
-        const newBoard = [...board]; 
-        newBoard[row][col] = pieceMove;
-        newBoard[firstMove.row][firstMove.col] = "";
-        setBoard(newBoard);
-        setPieceMove("");
-        setFirstMove({row:null, col:null})
-    }
-    else if(board[row][col] === "" && validMove  && !haveInteraction){
-        const newBoard = [...board]; 
-        newBoard[row][col] = pieceMove;
-        newBoard[firstMove.row][firstMove.col] = "";
-        setBoard(newBoard);
-        setPieceMove("");
-        setFirstMove({row:null, col:null});
+    if(haveInteraction){
+        alert("illegal move");
     }
     else{
-        alert("illegal move")
+        if(board[row][col] !== "" && !board[row][col].includes(pieceMove[0]) && validMove){
+            const newBoard = [...board]; 
+            newBoard[row][col] = pieceMove;
+            newBoard[firstMove.row][firstMove.col] = "";
+            setBoard(newBoard);
+            setPieceMove("");
+            setFirstMove({row:null, col:null})
+        }
+        else if(board[row][col] === "" && validMove){
+            const newBoard = [...board]; 
+            newBoard[row][col] = pieceMove;
+            newBoard[firstMove.row][firstMove.col] = "";
+            setBoard(newBoard);
+            setPieceMove("");
+            setFirstMove({row:null, col:null});
+        }
+        else{
+            alert("illegal move")
+        }
     }
     
 }
